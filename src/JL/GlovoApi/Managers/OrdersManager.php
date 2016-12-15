298a9ad712a2efc2c08d3436a943ee54d32def90
Joan Lopez <joanjan14@gmail.com>
2016-12-15 12:23:42 +0100

    Some refactors were done

--- src/JL/GlovoApi/Managers/OrdersManager.php
+++ src/JL/GlovoApi/Managers/OrdersManager.php
@@ -23,25 +23,12 @@ class OrdersManager
     {
         $url = sprintf(self::GET_ORDERS, $customerUrn);
         $response = $this->httpRequester->getJsonAuthorized($url, $clientToken);
-
         $orders = array();
         if (!$response->wasSuccessful()) return $orders;
-
         foreach ($response->parameters() as $order_data) {
-            $tmp_order = new Order(
-                $order_data->{'customer'}->{'urn'},
-                $order_data->{'description'},
-                $order_data->{'cityCode'},
-                $order_data->{'subtype'},
-                $order_data->{'points'}[0]->{'address'}->{'label'},
-                $order_data->{'points'}[0]->{'type'}
-            );
-            if (!is_null($order_data->{'urn'}))
-                $tmp_order->setUrn($order_data->{'urn'});
-
+            $tmp_order = $this->createOrderModel($order_data);
             array_push($orders, $tmp_order);
         }
-
         return $orders;
     }
 
@@ -49,28 +36,43 @@ class OrdersManager
     {
         $url = sprintf(self::GET_ORDER, $customerUrn, $orderUrn);
         $response = $this->httpRequester->getJsonAuthorized($url, $clientToken);
-
         if (!$response->wasSuccessful()) return null;
-        $tmp_order = new Order(
-            $response->parameters()->{'customer'}->{'urn'},
-            $response->parameters()->{'description'},
-            $response->parameters()->{'cityCode'},
-            $response->parameters()->{'subtype'},
-            $response->parameters()->{'points'}[0]->{'address'}->{'label'},
-            $response->parameters()->{'points'}[0]->{'type'}
-        );
-        if (!is_null($response->parameters()->{'urn'}))
-            $tmp_order->setUrn($response->parameters()->{'urn'});
-
+        $tmp_order = $this->createOrderModel($response->parameters());
         return $tmp_order;
     }
 
     public function createOrder($clientToken, $customerUrn, $description, $cityCode, $address, $addressType, $subtype)
     {
         $url = sprintf(self::POST_ORDER, $customerUrn);
+        $parameters = $this->createOrderRequestData($description, $cityCode, $address, $addressType, $subtype);
+        $response = $this->httpRequester->postJsonAuthorized($url, $clientToken, $parameters);
+        if (!$response->wasSuccessful()) return null;
+        $tmp_order = $this->createOrderModel($response->parameters());
+        return $tmp_order;
+    }
+
+    private function createOrderModel($order_data)
+    {
+        $tmp_order = new Order(
+            $order_data->{'customer'}->{'urn'},
+            $order_data->{'description'},
+            $order_data->{'cityCode'},
+            $order_data->{'subtype'},
+            $order_data->{'points'}[0]->{'address'}->{'label'},
+            $order_data->{'points'}[0]->{'type'}
+        );
+        if (!is_null($order_data->{'urn'}))
+        {
+            $tmp_order->setUrn($order_data->{'urn'});return $tmp_order;
+        }
+        return $tmp_order;
+    }
+
+    private function createOrderRequestData($description, $cityCode, $address, $addressType, $subtype)
+    {
         $parameters = array(
             array(
-                'address'=> array('label'=>$address),
+                'address' => array('label' => $address),
                 'type' => $addressType
             )
         );
@@ -78,21 +80,7 @@ class OrdersManager
         $parameters['cityCode'] = $cityCode;
         $parameters['points'] = array();
         $parameters['subtype'] = $subtype;
-        $response = $this->httpRequester->postJsonAuthorized($url, $clientToken, $parameters);
-
-        if (!$response->wasSuccessful()) return null;
-        $tmp_order = new Order(
-            $response->parameters()->{'customer'}->{'urn'},
-            $response->parameters()->{'description'},
-            $response->parameters()->{'cityCode'},
-            $response->parameters()->{'subtype'},
-            $response->parameters()->{'points'}[0]->{'address'}->{'label'},
-            $response->parameters()->{'points'}[0]->{'type'}
-        );
-        if (!is_null($response->parameters()->{'urn'}))
-            $tmp_order->setUrn($response->parameters()->{'urn'});
-
-        return $tmp_order;
+        return $parameters;
     }
 
 
